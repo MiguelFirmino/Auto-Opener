3208d7d08e996e345cb0843cf719f3adae33d85b
Miguel Firmino <miguel.firmino@yahoo.com>
2024-05-27 23:48:12 -0300

    Added the main.py file

---
+++ main.py
@@ -0,0 +1,42 @@
+from time import sleep
+from pyautogui import hotkey, write, press
+from pygetwindow import getWindowsWithTitle
+
+user_path = "C:/Users/F248584/"
+desktop_path = "C:/Users/Public/Desktop/"
+
+class Program:
+  def __init__(self, path, filename, title):
+    self.path = path
+    self.filename = filename
+    self.fullpath = self.path + self.filename
+    self.title = title
+
+programs = [
+  Program(user_path, "TEXTOS PADRÃO NOTAS.txt", "TEXTOS PADRÃO NOTAS"),
+  Program(user_path, "TICKETS TRATADOS v1.4.1.xlsx", "TICKETS TRATADOS"),
+  Program(user_path, "TEXTOS PADRÃO v1.1.0.doc", "TEXTOS PADRÃO"),
+  Program("", "snippingtool", "Ferramenta de Captura"),
+  Program(desktop_path, "MenuAplic.lnk", "Menu Principal"),
+  Program(desktop_path, "Nice Cluster2.lnk", "My Profile"),
+  Program(desktop_path, "Orhganiza - Google Chrome.exe", "Portal Orhganiza")
+]
+
+def open_program(program):
+  hotkey("win", "r")
+  write(program.fullpath)
+  press("enter")
+  sleep(0.05)
+
+check_if_closed = lambda x: len(getWindowsWithTitle(x.title)) == 0
+
+if __name__ == "__main__":
+  for program in programs:
+    try:
+      if check_if_closed(program):
+        print(f"Opening program: {program.filename}")
+        open_program(program)
+      else:
+        print(f"Program: {program.filename} is already open")
+    except:
+      pass
